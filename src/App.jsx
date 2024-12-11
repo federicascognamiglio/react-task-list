394b656f84844a9ce0612e0cfc05fbd4c9ae0c74
federicascognamiglio <scognamigliofede99@gmail.com>
2024-12-11 16:49:54 +0100

    feat: tasks filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import tasks from "../tasks.js"
-console.log(tasks);
-
 
 function App() {
+  // Data
+  const currentTasks = tasks.filter(curTask => curTask.state === "backlog" || curTask.state === "in_progress");
+  const completedTasks = tasks.filter(curTask => curTask.state === "completed");
   
-
+  // Functions
+  // const printTasks = () => currentTasks
   return (
     <>
-    
+      <header>
+        <h1>Task Manager</h1>
+      </header>
+      <main>
+        <div>
+          <h2>Current tasks</h2>
+          <ul></ul>
+        </div>
+
+      </main>
     </>
   )
 }
