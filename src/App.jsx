0fbe8f55a91d3dc5ab8f33a37c3a0d36ab880107
federicascognamiglio <scognamigliofede99@gmail.com>
2024-12-11 17:05:58 +0100

    feat: tasks printed

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,18 @@ function App() {
   // Data
   const currentTasks = tasks.filter(curTask => curTask.state === "backlog" || curTask.state === "in_progress");
   const completedTasks = tasks.filter(curTask => curTask.state === "completed");
-  
+
   // Functions
-  // const printTasks = () => currentTasks
+  const printTasks = (tasksArray) => tasksArray.map(curTask =>
+    <li key={curTask.id}>
+      {curTask.title} <span>{curTask.state}</span>
+      <ul>
+        <li key="{curTask.id}.1">Priority: {curTask.priority}</li>
+        <li key="{curTask.id}.2">Est. time {curTask.estimatedTime}</li>
+      </ul>
+    </li>)
+
+  // Output
   return (
     <>
       <header>
@@ -14,10 +23,13 @@ function App() {
       </header>
       <main>
         <div>
-          <h2>Current tasks</h2>
-          <ul></ul>
+          <h2>Current tasks ({currentTasks.length})</h2>
+          <ul>{printTasks(currentTasks)}</ul>
         </div>
-
+        <div>
+          <h2>Completed Tasks ({completedTasks.length})</h2>
+          <ul>{printTasks(completedTasks)}</ul>
+        </div>
       </main>
     </>
   )
